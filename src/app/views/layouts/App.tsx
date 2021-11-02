04a735262d00e56ca95339e53d9cfb16610aedb7
Vladislav Mikheev <vmikheev@primeaspect.io>
2021-11-02 09:58:43 +0300

    [feature-add_ReactFinalForm] Modified App.tsx file by removing useSate hook

--- src/app/views/layouts/App.tsx
+++ src/app/views/layouts/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createUser } from '../../state/ducks/user/userSlice';
 import { IAppState, RootState } from '../../state/store';
